86443cbb85b5830f33869a41027a741082e81932
Rohil Singh Dhillon <64743078+Rohil1710@users.noreply.github.com>
2022-12-07 23:46:19 -0700

    Done with explanation of how everything is related

--- sqlscript/queries.sql
+++ sqlscript/queries.sql
@@ -2,8 +2,49 @@ USE MUSEUM;
 
 -- 1) Show all tables and explain how they are related to one another (keys, triggers, etc.)
 SHOW TABLES;
+-- TABLES & KEYS:
+--
+-- The tables present in the relational schema are ART_OBJECT, PAINTING, STATUE, SCULPTURE, OTHER, PERMANENT COLLECTION, BORROWED, 
+-- COLLECTIONS, EXHIBITIONS, ARTIST, IN_COLLECTION, ON_DISPLAY
+--
+-- The table ART_OBJECT has primary id_no used to uniquely identify each art object in the database. It contains the following other attributes: the origin of the 
+-- art object (Origin), title of the art object (title), Epoch, a short description of the art object (Description), and the Year it was made (Year).
+--
+-- The table PAINTING has the foreign key id_no that points to ART_OBJECT. It contains the following other attributes about paintings present in the database:
+-- paint_type which is the type of painting, Drawn_on which is the year/date the painting was drawn, and the style of painting.
+--
+-- The table SCULPTURE has the foreign key id_no that points to ART_OBJECT, It contains the following other attributes about sculptures present in the database
+-- Material, Height, Weight, and Style. 
+--
+-- The table STATUE has the foreign key id_no that points to ART_OBJECT. It contains the following other attributes about statues present in the database
+-- Material, Height, Weight, and Style.
 -- 
-
+-- The table OTHER has the foreign key id_no that points to ART_OBJECT. It contains informaiton about other types of art objects in the database
+-- such as photos and prints. It contains the following other attributes to describe these art objects: type, style. 
+--
+-- The table COLLECTIONS contains the primary key NAME which is the name of the art collection in a museum or otherwise. It also contains the following other attributes 
+-- Phone (Phone number of the museum hosting the art collection or other institution), Contact.person (the person to contact in the museum hosting the collection),
+-- Street.address (the street address of where the collection is being hosted), City (the city in which the collection exists), Country (country in which the collection exists), 
+-- Postal_code, Type (the type of art being shown in the collection), Description (a short description of the collection).
+-- 
+-- The table EXHIBITIONS has the primary key NAME which is the name of the art exhibition. It contains the attributes Start.date (the starting date of the exhibition) and
+-- End_Date (which is the day the exhibition ends).
+--
+-- The table ARTIST contains the foreign key id_no which is used to identify what art object the artist is responsible for making, the primary key Name which is 
+-- the name of the artist. It contains the following other  other attributes as well: Description (Description of the artist), Date.born, Date.died, Country_of_origin, 
+-- Epoch, Main_style.
+--
+-- The table PERMANENT_cOLLECTION contains the foreign key id_no that points to ART_OBJECT and is used to store art objects permanently in a collection.
+-- It also contains the following other attributes: Status, Cost, and Date.acquired.
+--
+-- The table IN_COLLECTION contains the foreign keys id_no that points to ART_OBJECT and Name that points to COLLECTIONS. It is used to store art objects
+-- that are in a collection currently.
+--
+-- The table ON_DISPLAY contains the foreign keys id_no that points to ART_OBJECT and Name that points to EXHIBITIONS. It is used to store art objects that
+-- are currently on display in an exhibition.
+--
+-- TRIGGERS:
+-- Two triggers were used between the tables IN_COLLECTION and ON_DISPLAY. The triggers used were 'Update' and 'Delete'.
 -- 2) A basic retrieval query
 SELECT Name, Description, Date_born, Date_died
 FROM ARTIST
